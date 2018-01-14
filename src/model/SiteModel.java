e4e3d4fe6034763a94d1a3210d501a887f602a01
Matthew Hunt <2015huntmj@gmail.com>
2018-01-13 21:10:37 -0500

    modifies the site model to include data members and getters/setters

--- src/model/SiteModel.java
+++ src/model/SiteModel.java
@@ -1,5 +1,69 @@
+/**
+ * 
+ * 
+ * 
+ */
 package model;
 
 public class SiteModel {
-
+	
+	///
+	/// declare local fields
+	///
+	
+	private int id;				// holds id of site
+	private int num;			// holds site number
+	private String name;		// holds the name of the site
+	private String shortDesc;	// holds a short description of the site
+	private String loc;			// holds the location of the site
+	private long lat;			// holds the latitude of the site
+	private long lng;			// holds the longitude of the site
+	
+	///
+	/// getters and setters
+	///
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getShortDesc() {
+		return shortDesc;
+	}
+	public void setShortDesc(String shortDesc) {
+		this.shortDesc = shortDesc;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	public long getLat() {
+		return lat;
+	}
+	public void setLat(long lat) {
+		this.lat = lat;
+	}
+	public long getLng() {
+		return lng;
+	}
+	public void setLng(long lng) {
+		this.lng = lng;
+	}
+	
 }
