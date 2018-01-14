40eca400272f2a79d08330ba4eb3b4e6244d5b41
Matthew Hunt <2015huntmj@gmail.com>
2018-01-14 17:48:54 -0500

    adds READ operation for a single site to the controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -12,115 +12,189 @@
 //
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import model.DbConnect;
 import model.HistoryModel;
 import model.SiteModel;
 import view.SiteView;
 
 public class Controller {
-	
-	private SiteModel site;  
+
+	private SiteModel site;
 	private HistoryModel history;
 	private SiteView view;
 	private DbConnect db;
-	
+
+	private final String TABLE_SITE = "site";
+	private final String COLUMN_ID = "id";
+	private final String COLUMN_SITE_NUM = "num";
+	private final String COLUMN_NAME = "name";
+	private final String COLUMN_SHORT_DESC = "short_desc";
+	private final String COLUMN_LOCATION = "loc";
+	private final String COLUMN_LATITUDE = "lat";
+	private final String COLUMN_LONGITUDE = "lng";
+
+	// private final String PARAMETER_FIRST = "@" + COLUMN_FIRST;
+	// private final String PARAMETER_LAST = "@" + COLUMN_LAST;
+	// private final String PARAMETER_PAY_RATE = "@" + COLUMN_PAY_RATE;
+	private final String PARAMETER_ID = "@" + COLUMN_ID;
+	// private final String connectionString = "Data Source = |DataDirectory|\\" +
+	// DB_NAME + "; Version = 3";
+
 	// where the magic happens
 	public static void main(String[] args) {
 		Controller c = new Controller(null, null);
-		
+		SiteModel s = c.getSite(100);
+		System.out.println(s);
 	}
-	
+
 	// call model AND view
 	// constructor
 	public Controller(SiteModel site, SiteView view) {
 		db = new DbConnect();
-		db.getRemoteConnection();
+
 		this.site = site;
 		this.view = view;
 	}
-	
+
 	/* CRUD operation for Site */
 	// CREATE. add a site. view calls createSite from the controller
 	public void createSite(SiteModel s) {
-		
+
 	}
-	
+
 	// READ. browse a site if you want to get 1 site return 'only one site'.
 	public SiteModel getSite(int siteNum) {
+
+		///
+		/// declare local variables
+		///
 		
-		return null;
+		SiteModel result;		// holds the site retrieved from the database
+		Connection conn;		// holds the connection to the database
+		String query;			// holds query string
+		PreparedStatement stmt;	// holds Prepared Statement to execute on the database
+		ResultSet rs;			// holds the result from the database
+		
+		// initialize variables
+		result = new SiteModel();
+		conn = null;
+		query = null;
+		stmt = null;
+		rs = null;
+
+		try {
+			
+			// connect to the database
+			conn = db.getRemoteConnection();
+			
+			// concatenate select query
+			query = "SELECT * FROM " + TABLE_SITE + " WHERE " + COLUMN_SITE_NUM + " = " + " ?;";
+			
+			// initialize the prepare statement, execute it, and
+			// store the result
+			stmt = conn.prepareStatement(query);
+			stmt.setInt(1, siteNum);
+			rs = stmt.executeQuery();
+			rs.next();
+			
+			// store the result from the database in the site object
+			result.setId(rs.getInt(COLUMN_ID));
+			result.setNum(rs.getInt(COLUMN_SITE_NUM));
+			result.setName(rs.getString(COLUMN_NAME));
+			result.setShortDesc(rs.getString(COLUMN_SHORT_DESC));
+			result.setLoc(rs.getString(COLUMN_LOCATION));
+			result.setLat(rs.getLong(COLUMN_LATITUDE));
+			result.setLng(rs.getLong(COLUMN_LONGITUDE));
+
+		} catch (SQLException ex) {
+			
+			// display any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+			
+		} finally {
+			
+			// try to close the connection to the database
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException ignore) {
+
+				}
+			}
+		}
+		
+		// return the site retrieved from the database
+		return result;
 	}
+
 	// READ. browse sites. return all the 'sites' which is an array of site objects
 	public SiteModel[] getSites() {
 		return null;
 	}
-	
-	// UPDATE. edit and change a site. 
+
+	// UPDATE. edit and change a site.
 	public void updateSite(SiteModel s, int SiteNum) {
-		//view.printDetails(model.getSites(), model.getSite(SiteNum));
+		// view.printDetails(model.getSites(), model.getSite(SiteNum));
 	}
-	
+
 	// DELETE. delete a site.
 	public void deleteSite(int SiteNum) {
-	
+
 	}
-	
+
 	/* CRUD operation for History */
 	// CREATE. add a history item.
 	public void createHistoryItem(HistoryModel h) {
-		
+
 	}
-	
+
 	// READ. browse history items. we can get all of history items at a time.
 	public HistoryModel[] getHistoryItems() {
-		//History[] h = {a, b, c, ..., g};
+		// History[] h = {a, b, c, ..., g};
 		return null;
 	}
-	
+
 	// READ. browse a single history item.
 	public HistoryModel getHistoryItem() {
 		return null;
 	}
-	
-	// we do not need to UPDATE because we should update once a while. 
-	
-	//DELETE. 
+
+	// we do not need to UPDATE because we should update once a while.
+
+	// DELETE.
 	public void deleteHistoryItem(int historyID) {
-		
+
 	}
-	
+
 	// updateMap using GoogleStaticAPI and return to the view
 	public void updateMap() {
-	
+
 	}
-	
-	
-	
-	
-	/*//without DB 
-	public static void main(String[] args) {
-		
-		//
-		Site model = SiteInfoFromDB();
-		//
-		History model = HistoryInfoFromDB();
-		//
-		Site view = new Site();
-		
-		updateSite(Sites, int SiteNum);
-		updateMap();
-		
-		
-	}
-	
-	 private static Site SiteInfoFromDB() {
-		
-		int siteNum;
-		Site site = new Site();
-		site
-		site.getSite(siteNum);
-		return site;
-		
-	}
-	*/
+
+	/*
+	 * //without DB public static void main(String[] args) {
+	 * 
+	 * // Site model = SiteInfoFromDB(); // History model = HistoryInfoFromDB(); //
+	 * Site view = new Site();
+	 * 
+	 * updateSite(Sites, int SiteNum); updateMap();
+	 * 
+	 * 
+	 * }
+	 * 
+	 * private static Site SiteInfoFromDB() {
+	 * 
+	 * int siteNum; Site site = new Site(); site site.getSite(siteNum); return site;
+	 * 
+	 * }
+	 */
 }
