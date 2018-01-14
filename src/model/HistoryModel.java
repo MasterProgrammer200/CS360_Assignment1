d273779654813543de01ca519e0eff12cd013bf3
Matthew Hunt <2015huntmj@gmail.com>
2018-01-13 21:22:16 -0500

    modifies the history model to include data members and getters/setters

--- src/model/HistoryModel.java
+++ src/model/HistoryModel.java
@@ -1,5 +1,50 @@
+/**
+ * 
+ * 
+ * 
+ */
 package model;
 
-public class HistoryModel {
+import java.util.Date;
 
+public class HistoryModel {
+	
+	///
+	/// declare local fields
+	///
+	
+	private int id;				// holds id of history item
+	private int siteNum;		// holds site number associated with this history item
+	private char action;		// holds the action performed for this history item
+	private Date date;			// holds the date this history item was created
+	
+	///
+	/// getters and setters
+	///
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getSiteNum() {
+		return siteNum;
+	}
+	public void setSiteNum(int siteNum) {
+		this.siteNum = siteNum;
+	}
+	public char getAction() {
+		return action;
+	}
+	public void setAction(char action) {
+		this.action = action;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
 }
